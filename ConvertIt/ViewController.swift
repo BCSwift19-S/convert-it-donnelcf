d4c8029d85b900daff03c88442eb180a649ba7b1
Christopher Donnelly <christopherdonnelly@christophers-macbook.local>
2019-02-23 16:47:16 -0500

    6.4 Complete

--- ConvertIt/ViewController.swift
+++ ConvertIt/ViewController.swift
@@ -23,11 +23,40 @@ class ViewController: UIViewController {
                         "Meters to Yeards"]
     var fromUnits = ""
     var toUnits = ""
+    var conversionString = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
         formulaPicker.delegate = self
         formulaPicker.dataSource = self
+    }
+    
+    func calculateConversion() {
+        var outputValue = 0.0
+        if let inputValue = Double(userInput.text!) {
+            
+            switch conversionString {
+            case "Miles to Kilometers":
+                outputValue = inputValue / 0.62137
+            case "Kilometers to Miles":
+                outputValue = inputValue * 0.62137
+            case "Feet to Meters":
+                outputValue = inputValue / 3.2808
+            case "Yards to Meters":
+                outputValue = inputValue / 1.0936
+            case "Meters to Feet":
+                outputValue = inputValue * 3.2808
+            case "Meters to Yeards":
+                outputValue = inputValue * 1.0936
+            default:
+                print("show alert for bad conversion string")
+            }
+            resultsLabel.text = "\(inputValue) \(fromUnits) = \(outputValue) \(toUnits)"
+        } else {
+            print("value entered was not a number")
+        }
+        
+        
     }
     
     
@@ -52,10 +81,12 @@ extension ViewController: UIPickerViewDelegate, UIPickerViewDataSource {
     }
     
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        conversionString = formulaArray[row]
         let unitsArray = formulaArray[row].components(separatedBy: " to ")
         fromUnits = unitsArray[0]
         toUnits = unitsArray[1]
         fromUnitsLabel.text = fromUnits
+        calculateConversion()
     }
     
     
